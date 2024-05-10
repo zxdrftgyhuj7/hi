26ff0bfa8608f0d89ba4f743ab70598f3c59d5d6
zxdrftgyhuj7 <gs1lolzkiki@hotmail.com>
2024-05-10 14:32:54 +0700

    update aptos commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
+function countSubstrings(s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    extendPalindrome(i, i);
+    extendPalindrome(i, i + 1);
   }
-  return -1;
+  return count;
+  function extendPalindrome(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+  }
 }
