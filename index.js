61af56ad9fa81d4ded33e3d16b515aff5d590469
zxdrftgyhuj7 <gs1lolzkiki@hotmail.com>
2024-05-15 14:00:36 +0700

    update sdk

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-function minimumTotal(triangle) {
-  const n = triangle.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = 0; j <= i; j++) {
-      dp[j] = triangle[i][j] + Math.min(dp[j], dp[j + 1]);
+function uniquePathsWithObstacles(obstacleGrid) {
+  const m = obstacleGrid.length;
+  const n = obstacleGrid[0].length;
+  const dp = Array.from(Array(m), () => Array(n).fill(0));
+  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (obstacleGrid[i][j] === 1) continue;
+      if (i > 0) dp[i][j] += dp[i - 1][j];
+      if (j > 0) dp[i][j] += dp[i][j - 1];
     }
   }
-  return dp[0];
+  return dp[m - 1][n - 1];
 }
