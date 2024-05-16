3d6ee8963892dd6bcdc1085d95f686e76a7fdf40
zxdrftgyhuj7 <gs1lolzkiki@hotmail.com>
2024-05-16 12:36:01 +0700

    aptos

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function uniquePathsWithObstacles(obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
-  const dp = Array.from(Array(m), () => Array(n).fill(0));
-  if (obstacleGrid[0][0] === 0) dp[0][0] = 1;
-  for (let i = 0; i < m; i++) {
-    for (let j = 0; j < n; j++) {
-      if (obstacleGrid[i][j] === 1) continue;
-      if (i > 0) dp[i][j] += dp[i - 1][j];
-      if (j > 0) dp[i][j] += dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+function countConsonants(str) {
+  return str.match(/[bcdfghjklmnpqrstvwxyz]/gi).length;
 }
